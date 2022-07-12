15de6a6c54d5fafbf17040a792b51ca684f6e9f2
_Leg3ndary <ben.zhou.2006@gmail.com>
2022-07-11 22:19:23 -0400

    Finally, jesus

---
+++ scripts/processTagScript.js
@@ -0,0 +1,44 @@
+// Out of respect, please don't go spamming my API :(
+
+const API_URL = "https://mighty-sea-55702.herokuapp.com/https://btp.leg3ndary.repl.co/process/";
+
+class Response {
+    constructor(data) {
+        this.body = data.body;
+        this.actions = data.actions;
+        this.extras = data.extras;
+    }
+}
+
+const button = document.getElementById("processBTN");
+
+function escapeHtml(unsafe) {
+    // ty asport
+    return unsafe
+        .replace(/&/g, "&amp;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;")
+        .replace(/"/g, "&quot;")
+        .replace(/'/g, "&#039;");
+}
+
+button.addEventListener('click', event => {
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+    let tagscript = document.getElementById("tagscript").value;
+    let request = encodeURIComponent(tagscript);
+
+    let response = fetch(API_URL + request, {
+        headers: headers
+    })
+    .then(res => res.json());
+
+    console.log(response);
+
+    response.then(function(resp) {
+        console.log(resp);
+        response = new Response(resp);
+        document.getElementById("output").value = response.body;
+    });
+});
