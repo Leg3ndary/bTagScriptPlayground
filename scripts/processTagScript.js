0fb6c57ffa152ccd8fe1229507fb45b15c03d924
_Leg3ndary <ben.zhou.2006@gmail.com>
2022-07-14 16:42:56 -0400

    Push some new features!

--- scripts/processTagScript.js
+++ scripts/processTagScript.js
@@ -46,23 +46,87 @@ function escapeHtml(unsafe) {
 }
 
 function loadActionTable(actions) {
-    const table = document.getElementById("actionBody");
+    let table = document.getElementById("actionBody");
+    let errorsText = document.getElementById("errors");
+    let warningsText = document.getElementById("warnings");
 
     while(table.hasChildNodes()){
         table.removeChild(table.firstChild);
     }
+    errorsText.innerText = "Errors: None";
+    warningsText.innerText = "Warnings: None";
+
+    let errors = [];
+    let warnings = [];
 
     for (const [action, value] of Object.entries(actions)) {
         let row = table.insertRow();
-
         let actionRow = row.insertCell(0);
-        actionRow.innerHTML = escapeHtml(action);
-        
         let valuesRow = row.insertCell(1);
-        valuesRow.innerHTML = escapeHtml(JSON.stringify(value));
-
         let badge = row.insertCell(2);
-        badge.innerHTML = `<div class="badge badge-outline-success">Success</div>`;
+
+        let actionContent = "Error";
+        let valueContent = "Error";
+        let badgeContent = `<div class="badge badge-danger">Error</div>`;
+
+        if ( action == "blacklist" ) {
+            if ( actions.hasOwnProperty("requires") ) {
+                errors.push("You cannot have both a blacklist and a require block.");
+                actionContent = action;
+                valueContent = `Blacklisting the following IDS: ${value.items.join(", ")}, replying with "${value.response}" when the user/role/channel is blacklisted.`;
+                badgeContent = `<div class="badge badge-danger">Error</div>`;
+            } else {
+                valueContent = `Blacklisting the following IDS: ${value.items.join(", ")}, replying with "${value.response}" when the user/role/channel is blacklisted.`;
+                badgeContent = `<div class="badge badge-success">Success</div>`;
+            };
+        } else if ( action == "commands" ) {
+            actionContent = "command";
+            valueContent = `The following commands are used with their associated values: ${value.join(", ")}`;
+            badgeContent = `<div class="badge badge-success">Success</div>`;
+        } else if ( action == "delete" ) {
+            actionContent = "delete";
+            valueContent = `Deleting the message`;
+            badgeContent = `<div class="badge badge-success">Success</div>`;
+        } else if ( action == "embed" ) {
+            actionContent = "embed";
+            valueContent = `Embedding the following embed json: ${JSON.stringify(value)}`;
+            badgeContent = `<div class="badge badge-success">Success</div>`;
+        } else if ( action == "overrides" ) {
+            actionContent = "override";
+            valueContent = `Overriding command permissions.`;
+            badgeContent = `<div class="badge badge-success">Success</div>`;
+        } else if ( action == "reactions" ) {
+            actionContent = "reaction";
+            valueContent = `Adding the following reactions: ${value.join(", ")}`;
+            badgeContent = `<div class="badge badge-success">Success</div>`;
+        } else if ( action == "requires" ) {
+            if ( actions.hasOwnProperty("requires") ) {
+                actionContent = "require";
+                valueContent = `Requiring the following IDS: ${value.items.join(", ")}, replying with "${value.response}" when the requirements aren't met.`;
+                badgeContent = `<div class="badge badge-danger">Error</div>`;
+            } else {
+                actionContent = "require";
+                valueContent = `Requiring the following IDS: ${value.items.join(", ")}, replying with "${value.response}" when the requirements aren't met.`;
+                badgeContent = `<div class="badge badge-success">Success</div>`;
+            };
+        } else if ( action == "target" ) {
+            actionContent = "redirect";
+            valueContent = `Redirecting the message to the following channel: ${value.channel}`;
+            badgeContent = `<div class="badge badge-success">Success</div>`;
+        } else {
+            console.log("Unknown action: " + action);
+        };
+
+        actionRow.innerHTML = escapeHtml(actionContent);
+        valuesRow.innerHTML = escapeHtml(valueContent);
+        badge.innerHTML = badgeContent;
+    };
+
+    if ( errors.length > 0 ) {
+        errorsText.innerText = "Errors: " + errors.join(", ");
+    };
+    if ( warnings.length > 0 ) {
+        warningsText.innerText = "Warnings: " + warnings.join(", ");
     };
 }
 
