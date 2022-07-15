87771036010233ab553cd4d8959de93967eb90d9
Albert Portnoy <albert@albertp.dev>
2022-07-15 15:08:56 -0700

    Add eslint

--- scripts/importCarlScript.js
+++ scripts/importCarlScript.js
@@ -1,51 +1,51 @@
 function isInt(str) {
-    if (typeof str !== 'string') {
-        return false;
-    }
-    
-    const num = Number(str);
-    
-    if (Number.isInteger(num) && num > 0) {
-        return true;
-    }
-    
-    return false;
+	if (typeof str !== 'string') {
+		return false;
+	}
+
+	const num = Number(str);
+
+	if (Number.isInteger(num) && num > 0) {
+		return true;
+	}
+
+	return false;
 }
 
 
-const CARL_API_URL = "https://mighty-sea-55702.herokuapp.com/https://carl.gg/api/v1/tags/";
+const CARL_API_URL = 'https://mighty-sea-55702.herokuapp.com/https://carl.gg/api/v1/tags/';
 
 
-const carlImportURL = document.getElementById("carlImportURL");
-const carlImportBtn = document.getElementById("carlImport");
+const carlImportURL = document.getElementById('carlImportURL');
+const carlImportBtn = document.getElementById('carlImport');
 
 function parseID(str) {
-    if ( isInt(str) == true ) {
-        tagID = str;
-    } else {
-        tagID = str.split("/").pop();
-        if ( isInt(tagID) == false ) {
-            console.log("Add something here when tag isn't found idiot");
-            // Custom tag I made lul
-            tagID = 1479390;
-        }
-    }
-    return tagID;
+	if (isInt(str) == true) {
+		tagID = str;
+	} else {
+		tagID = str.split('/').pop();
+		if (isInt(tagID) == false) {
+			console.log('Add something here when tag isn\'t found idiot');
+			// Custom tag I made lul
+			tagID = 1479390;
+		}
+	}
+	return tagID;
 }
 
 async function importTag() {
 
-    let importURL = carlImportURL.value;
+	const importURL = carlImportURL.value;
 
-    let tagID = parseID(importURL);
-    
-    let resp = await fetch(CARL_API_URL + tagID).then(response => response.json());
-    editor.setValue(resp.content);
+	const tagID = parseID(importURL);
 
-    tagscript.innerText = "";
+	const resp = await fetch(CARL_API_URL + tagID).then(response => response.json());
+	editor.setValue(resp.content);
+
+	tagscript.innerText = '';
 }
 
 carlImportBtn.addEventListener('click', importTag);
 carlImportURL.addEventListener('keydown', e => {
-    if (e.key === "Enter") importTag();
+	if (e.key === 'Enter') importTag();
 });
